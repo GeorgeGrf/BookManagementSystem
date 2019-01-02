e0ab203f7210f501fb295be56d1512b848f8e3d3
GeorgeGrf <georgegaryf@gmail.com>
2019-01-02 18:41:40 +0200

    .

--- BooksManagement/src/gr/dit/hua/bms/controllers/LoginController.java
+++ BooksManagement/src/gr/dit/hua/bms/controllers/LoginController.java
@@ -1,5 +1,23 @@
 package gr.dit.hua.bms.controllers;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class LoginController {
 
+	public String simpleMethod( ) {
+		return "loginPage";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
