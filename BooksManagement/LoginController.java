d6a283b15d062f133c003253eb0efe3e09543ebb
GeorgeGrf <georgegaryf@gmail.com>
2019-01-03 02:53:50 +0200

    merged our work

---
+++ BooksManagement/LoginController.java
@@ -0,0 +1,25 @@
+package gr.dit.hua.bms.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+	public String simpleMethod( ) {
+		return "loginPage";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
--- BooksManagement/src/gr/dit/hua/bms/LoginServlet.java
+++ BooksManagement/src/gr/dit/hua/bms/LoginServlet.java
@@ -39,7 +39,6 @@ public class LoginServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("hi dude");
 		String name = request.getParameter("name");
 		String pwd = request.getParameter("password");
 		HttpSession session = request.getSession();
