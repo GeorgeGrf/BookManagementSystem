7e35fa3e750154f870fb5cd0912421d2927de527
ikouniaris <ikouniaris@gmail.com>
2019-02-25 12:13:26 +0200

    Secretary working

---
+++ BooksManagement/WebContent/WEB-INF/view/secretaryResults.jsp
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Results</title>
+</head>
+<body>
+Student name: ${studentName}<br>
+Course title: ${courseName}<br>
+State: ${state}<br>
+<form action="sBack" method="post"> 
+<Button type="submit">Back</Button>
+</form>
+</body>
+</html>
--- BooksManagement/src/gr/dit/hua/bms/controllers/SecretaryController.java
+++ BooksManagement/src/gr/dit/hua/bms/controllers/SecretaryController.java
@@ -8,20 +8,30 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-
+import gr.dit.hua.entities.Course;
 import gr.dit.hua.entities.State;
+import gr.dit.hua.entities.User;
 
 @Controller
 public class SecretaryController {
-
+	String subject;
+	
+	@RequestMapping("/sBack")
+	public String sBack(HttpServletRequest request) {
+		return "secretary";
+	}
+	
 	@RequestMapping("/searchState")
-	public String pickSubject(HttpServletRequest request) {
+	public String searchState(HttpServletRequest request, Model model) {
 		int studentId=Integer.parseInt(request.getParameter("studentId"));
 		int courseId=Integer.parseInt(request.getParameter("courseId"));
 		
-		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(State.class)
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(State.class).addAnnotatedClass(Course.class)
+				.addAnnotatedClass(User.class)
+
 				.buildSessionFactory();
 		Session session = factory.getCurrentSession();
 		try {
@@ -29,16 +39,35 @@ public class SecretaryController {
 			session.beginTransaction();
 			
 			// get the course object
-			List results = session
+			@SuppressWarnings("unchecked")
+			List<State> Stateresults = session
 		            .createQuery("select c from State c where studentId = :studentId AND courseId=:courseId")
 		            .setParameter("studentId", studentId).setParameter("courseId", courseId)
 		            .list();
-		
+			@SuppressWarnings("unchecked")
+			List<User> Studentresults= session
+		            .createQuery("select c from User c where id = :studentId")
+		            .setParameter("studentId", studentId)
+		            .list();
+			@SuppressWarnings("unchecked")
+			List<Course> Courseresults= session.createQuery("select c from Course c where courseId = :courseId")
+		            .setParameter("courseId", courseId)
+		            .list();
+			String studentName=Studentresults.get(0).getUsername();
+			String courseName=Courseresults.get(0).getTitle();
+			String state=Stateresults.get(0).getStatus();
+			
+			
+			
+			model.addAttribute("studentName", studentName);
+			model.addAttribute("courseName", courseName);
+			model.addAttribute("state", state);
+			return "secretaryResults";
 	} finally {
 		factory.close();
 		session.close();
 	}
 	
-	return "";
+	
 	
 }}
